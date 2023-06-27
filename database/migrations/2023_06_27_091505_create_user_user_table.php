7dbc9ebcc71ff9aa018a8ab0b429752e25a3400a
Kasra Salehvand <thekasra@outlook.com>
2023-06-27 12:51:19 +0330

    Created user_user table

---
+++ database/migrations/2023_06_27_091505_create_user_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('follower_id')->constrained('users', 'id');
+            $table->foreignId('following_id')->constrained('users', 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_user', function (Blueprint $table) {
+            $table->dropForeign(['follower_id']);
+            $table->dropForeign(['following_id']);
+        });
+
+        Schema::dropIfExists('user_user');
+    }
+};
