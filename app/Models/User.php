c6a726eecf5443feab5164887d697302ef0070c4
Kasra Salehvand <thekasra@outlook.com>
2023-06-27 14:25:18 +0330

    Updated User model, established many to many self-relationship.

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Publisher::class, 'publisher_user');
     }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_user');
+    }
 }
