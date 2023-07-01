85903be20c9abdc1399546189202b9be86b9881f
Kasra Salehvand <thekasra@outlook.com>
2023-07-01 20:24:30 +0330

    Added a custom middleware PreventBackHistory

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware(['auth', 'prevent-back-history'])->group(function () {
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
+});
+
 require __DIR__.'/auth.php';
