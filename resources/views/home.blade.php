18ca49b5cd5c6df72f855cde6268e0b8adaa47b2
Kasra Salehvand <thekasra@outlook.com>
2023-07-01 20:49:23 +0330

    Added 'partials' directory for views starting with _navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,34 +13,8 @@
     </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">شبکه اجتماعی کتاب</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/home">خانه</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/profile">پروفایل</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">دوستان</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        خروج
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </li>
-            </ul>
-        </div>
-    </nav>
+
+    @include('partials._navbar')
 
     <div class="container mt-4">
         <h1>به شبکه اجتماعی خوانندگان کتاب خوش آمدید</h1>
