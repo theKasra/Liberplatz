dd5d65aeb0e5ee982b4679d32fd37ec6957f4c15
Kasra Salehvand <thekasra@outlook.com>
2023-07-19 00:50:28 +0330

    Updated navbar partial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,36 +9,7 @@
 </head>
 <body>
 
-  <nav>
-    <div class="nav-right">
-      <img src="{{ asset('storage/images/logo.png') }}" alt="logo" class="logo">
-
-      <div class="nav-user-icon">
-        <img src="{{ asset('storage/images/profile-pic.png') }}" alt="user-img">
-      </div>
-
-      <ul>
-        <li><img src="{{ asset('storage/images/books.png') }}" alt="books-img"></li>
-        <li><img src="{{ asset('storage/images/people.png') }}" alt="people-img"></li>
-        <li>
-        <a class="nav-link" href="{{ route('logout') }}"
-          onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-            <img src="{{ asset('storage/images/logout.png') }}" alt="logout-img">
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          @csrf
-        </form>
-        </li>
-      </ul>
-    </div>
-
-    <div class="nav-left">
-      <div class="search-box">
-        <img src="{{ asset('storage/images/search.png') }}" alt="search-img">
-        <input type="text" placeholder="جستجو">
-      </div>
-    </div>
-  </nav>
+  @include('partials._navbar')
 
   <div class="container">
     <div class="right-sidebar">
--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,28 +1,32 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">شبکه اجتماعی کتاب</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/home">خانه</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/profile">پروفایل</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">دوستان</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('logout') }}"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    خروج
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-        </ul>
+<nav>
+    <div class="nav-right">
+      <a href="/home">
+        <img src="{{ asset('storage/images/logo.png') }}" alt="logo" class="logo">
+      </a>
+
+      <div class="nav-user-icon">
+        <img src="{{ asset('storage/images/profile-pic.png') }}" alt="user-img">
+      </div>
+
+      <ul>
+        <li><img src="{{ asset('storage/images/books.png') }}" alt="books-img"></li>
+        <li><img src="{{ asset('storage/images/people.png') }}" alt="people-img"></li>
+        <li>
+        <a class="nav-link" href="{{ route('logout') }}"
+          onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <img src="{{ asset('storage/images/logout.png') }}" alt="logout-img">
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+        </li>
+      </ul>
     </div>
-</nav>
+
+    <div class="nav-left">
+      <div class="search-box">
+        <img src="{{ asset('storage/images/search.png') }}" alt="search-img">
+        <input type="text" placeholder="جستجو">
+      </div>
+    </div>
+  </nav>
