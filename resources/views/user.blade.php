a6ff2219a31f266d8e0692486a18e9f94eefb1c1
Kasra Salehvand <thekasra@outlook.com>
2023-07-26 13:52:03 +0330

    Fixed profile detail and info UI
    made it into a partial view

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -15,71 +15,7 @@
 
   <div class="profile-container">
 
-    <div class="profile-details">
-      <div class="profile-detail-right">
-        <div class="profile-detail-row">
-          <img class="profile-detail-image" src="{{ asset('storage/images/profile-pic.png') }}" alt="user-profile">
-          <div>
-            <h3> {{ $user->first_name }} {{ $user->last_name }}</h3>
-            <h5 style="margin-right: 10px;"> {{ $user->name }}@</h5>
-            <!-- <p>دنبال کنندگان</p>
-                    <p>دنبال شوندگان</p> -->
-            <a href="{{ route('user.followings', ['id' => $user->id ]) }}">دنبال کنندگان: {{ $following_count }}</a>
-            <a href="{{ route('user.followers', ['id' => $user->id ]) }}">دنبال شوندگان: {{ $follower_count }}</a>
-          </div>
-        </div>
-      </div>
-
-      @if ($user->id != Auth::user()->id)
-        @if (!Auth::user()->isFollowing($user))
-          <div class="profile-detail-left">
-            <form action="{{ route('user.follow', ['id' => $user->id]) }}" method="post">
-              @csrf
-              <button type="submit">دنبال کن<img src="{{ asset('storage/images/follow.png') }}" alt=""></button>
-            </form>
-          </div>
-        
-        @else
-          <div class="profile-detail-left">
-            <form action="{{ route('user.unfollow', ['id' => $user->id]) }}" method="post">
-              @csrf
-              <button type="submit" style="background: #e4e6eb; color: black;">دنبال نکن<img src="{{ asset('storage/images/follow.png') }}" alt=""></button>
-            </form>
-          </div>
-        @endif
-
-      @endif
-    </div>
-
-    <div class="profile-info">
-
-      
-      <div class="info-column">
-        <div>
-          @include('partials._right-side-bar')
-        </div>
-        <div class="profile-intro">
-          <h3 style="margin-right: 30px; margin-bottom: 12px;">لیست های شخصی</h3>
-          <ul style="margin-right: 50px; margin-top: 5px;">
-            <li>
-              <a href="{{ route('user.favorites', ['id' => $user->id]) }}" style="text-decoration: none;">
-                کتاب های موردعلاقه
-              </a>
-            </li>
-            <li>
-              <a href="" style="text-decoration: none;">
-                دیدگاه ها
-              </a>
-            </li>
-            <li>
-              <a href="" style="text-decoration: none;">
-                بریده ها
-              </a>
-            </li>
-          </ul>
-          <hr>
-        </div>
-      </div>
+    @include('partials._profile-detail-info')
 
       <div class="post-column">
 
