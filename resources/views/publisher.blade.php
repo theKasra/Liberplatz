c107a234262033794abb11d780ec1aaba8e7c996
Kasra Salehvand <thekasra@outlook.com>
2023-07-28 01:56:09 +0330

    Fixed publisher view

--- resources/views/publisher.blade.php
+++ resources/views/publisher.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@ {{ $publisher->name }}</title>
+  <title>نشر {{ $publisher->name }}</title>
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <link rel="stylesheet" href="https://cdn.fontcdn.ir/Font/Persian/Vazir/v5.0.2/Vazir.css" type="text/css">
 </head>
